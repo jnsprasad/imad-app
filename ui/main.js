390a7bc19484a94fb1dc8884d82870464c279ed7
jnsprasad <30649523+jnsprasad@users.noreply.github.com>
2017-08-19 15:52:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 element.innerHTML = 'New vlaue';
+
+var img = document.getElementById('madi');
+madi.onclick = function() {
+    madi.style.marginleft = '100px';
+}
