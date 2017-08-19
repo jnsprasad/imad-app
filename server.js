ac689739c9370f237f95306a9f03ecf43bc777c7
jnsprasad <30649523+jnsprasad@users.noreply.github.com>
2017-08-19 15:17:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,66 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var Page1 = {
+  title: 'Page One | Siva',
+  heading: 'Page1',
+  date: 'August 19 2017',
+  content: `
+        <p>
+            Page1 is hosted from the server Good Luck. Page1 is hosted from the server Good Luck. Page1 is hosted from the server Good Luck. Page1 is hosted from the server Good Luck. Page1 is hosted from the server Good Luck. Page1 is hosted from the server Good Luck.
+        </p>
+        <p>
+            Page1 is hosted from the server Good Luck. Page1 is hosted from the server Good Luck. Page1 is hosted from the server Good Luck. Page1 is hosted from the server Good Luck. Page1 is hosted from the server Good Luck. Page1 is hosted from the server Good Luck.
+        </p>
+        <p>
+            Page1 is hosted from the server Good Luck. Page1 is hosted from the server Good Luck. Page1 is hosted from the server Good Luck. Page1 is hosted from the server Good Luck. Page1 is hosted from the server Good Luck. Page1 is hosted from the server Good Luck.
+        </p>`
+};
+
+function createTemplate (data){
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    
+    var htmlTemplate = `
+    <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/>
+                <h3>
+                    ${heading}
+                </h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${content}
+                    </p>
+                </div>
+            </div>
+        </body>
+    </html>
+    `;
+    return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/page1', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'page1.html'));
+  res.send(createTemplate(Page1));
 });
 
 app.get('/page2', function (req, res) {
