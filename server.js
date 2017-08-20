958c158234607511b105c8659fc5a03a07b91d8e
jnsprasad <30649523+jnsprasad@users.noreply.github.com>
2017-08-20 17:35:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -130,7 +130,7 @@ app.get('/pages/:pagenum', function (req,res) {
          if (result.rows.length === 0){
              res.status(404).send('Page not found');
          } else {
-             var pagedata = reult.rows[0];
+             var pagedata = result.rows[0];
              res.send(createTemplate(pagedata));
          }
      }
