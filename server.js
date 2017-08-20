972ddc74a3eb2af3301a23f25a003cec51f54a84
jnsprasad <30649523+jnsprasad@users.noreply.github.com>
2017-08-20 17:30:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -121,9 +121,20 @@ app.get('/submit-name', function(req, res) { // /submit-name?name=xxxx
    res.send(JSON.stringify(names)); //TODO
 });
 
-app.get('/:pagenum', function (req,res) {
-  var pageno = req.params.pagenum;
-  res.send(createTemplate(pages[pageno]));
+app.get('/pages/:pagenum', function (req,res) {
+  
+  pool.query("SELECT * FROM page WHERE title =" + req.params.pagenum, function(err,result){
+     if (err){
+         res.status(500).send(err.toString());
+     } else {
+         if (result.rows.length === 0){
+             res.status(404).send('Page not found');
+         } else {
+             var pagedata = reult.rows[0];
+             res.send(createTemplate(pagedata));
+         }
+     }
+  });
 });
 
 app.get('/ui/style.css', function (req, res) {
